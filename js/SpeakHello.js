5ffa619e691faaca2a8774e36a9b21ab8f3dd362
Mac <mac@MacBook-Air-Mac-2.local>
2022-07-23 15:43:24 +0300

    Module 4 assignment

---
+++ js/SpeakHello.js
@@ -0,0 +1,9 @@
+(function () {
+  var helloSpeaker = {};
+  var greeting = "Hello ";
+  helloSpeaker.sayHello = function (name) {
+    console.log(greeting + name);
+  };
+
+  window.helloSpeaker = helloSpeaker;
+})();
